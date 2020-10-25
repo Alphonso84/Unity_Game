a22d0a500ac8ade8e041a9e96b183d3fac90a0aa
Alphonso Sensley II <alphonso.sensleyjr@gmail.com>
2020-10-25 15:46:59 -0700

    changed logic

--- 2D Game/Assets/Bird.cs
+++ 2D Game/Assets/Bird.cs
@@ -1,7 +1,9 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Bird : MonoBehaviour
 {
+    bool _userIsDragging = false;
 
     Vector3 _initialPosition;
 
@@ -11,15 +13,31 @@ public class Bird : MonoBehaviour
 
     }
 
+    private void Update()
+    {
+        if (_userIsDragging) 
+        {
+
+        }
+        else if (transform.position.y > 10 || transform.position.y < -3.7)
+        {
+            
+            string currentSceneName = SceneManager.GetActiveScene().name;
+            SceneManager.LoadScene(currentSceneName);
+
+        }
+    }
+
     private void OnMouseDown()
     {
-        GetComponent<Rigidbody2D>().gravityScale = 0;
+        _userIsDragging = true;
         // GetComponent<SpriteRenderer>().color = Color.blue;
     }
 
     private void OnMouseUp() 
     {
         // GetComponent<SpriteRenderer>().color = Color.white;
+        _userIsDragging = false;
         Vector2 directionToInitialPosition = _initialPosition - transform.position;
         GetComponent<Rigidbody2D>().AddForce(directionToInitialPosition * 100);
         GetComponent<Rigidbody2D>().gravityScale = 1;
@@ -29,6 +47,7 @@ public class Bird : MonoBehaviour
     {
         Vector3 newPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         transform.position = new Vector3(newPosition.x, newPosition.y);
+        // GetComponent<Rigidbody2D>().gravityScale = 0;
     }
 
 }
